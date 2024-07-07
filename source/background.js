75c3c46feb214c48bdbf34e2737e55f758cf42ad
Omayeli Arenyeka <aomayeli@gmail.com>
2024-07-07 10:52:48 -0400

    Working on screenshot capability

--- source/background.js
+++ source/background.js
@@ -1,2 +1,23 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './options-storage.js';
+console.log('here')
+
+let screenshots = [];
+let intervalID;
+
+// Listen for a click on the camera icon. On that click, take a screenshot.
+chrome.action.onClicked.addListener(async function () {
+    clearInterval(intervalID)
+});
+
+function startUp() {
+    async function getScreenshot(){
+        screenshots.push(await chrome.tabs.captureVisibleTab());
+        console.log(screenshots)
+    }
+    intervalID = setInterval(getScreenshot, 500);
+}
+
+// Ensure the background script always runs.
+chrome.runtime.onStartup.addListener(startUp)
+chrome.runtime.onInstalled.addListener(startUp)
